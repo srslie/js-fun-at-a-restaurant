c45e32039ad07b6a53dd8bdab755b15d88a423b5
Alice Ruppert <aliceruppert@gmail.com>
2020-10-09 02:47:10 -0600

    Add passing meal-test.js

--- src/Chef.js
+++ src/Chef.js
@@ -1,6 +1,5 @@
 class Chef {
   constructor() {
-    
   }
 }
 
--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,47 @@
-function nameMenuItem() {
-
+function nameMenuItem(item) {
+  return `Delicious ${item}`;
 }
+
+function createMenuItem(name, price, type) {
+  var menuItem = {
+    name: name,
+    price: price,
+    type: type,
+  }
+  return menuItem;
+}
+
+function addIngredients(item, ingredients) {
+  if (!ingredients.includes(item)) {
+   ingredients.push(item)};
+ };
+
+function formatPrice(price) {
+  return `$${price}`;
+};
+
+function decreasePrice(price) {
+  return price * .9;
+};
+
+function createRecipe(title, ingredients, type) {
+  var recipe = {
+    title: title,
+    ingredients: ingredients,
+    type: type,
+  }
+  return recipe
+};
+
+
+
 
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
