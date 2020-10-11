235370884250109f51ea013a9b0b812943cefff2
Alice Ruppert <aliceruppert@gmail.com>
2020-10-10 23:04:21 -0600

    Add passing chef.js

--- src/Chef.js
+++ src/Chef.js
@@ -1,6 +1,24 @@
 class Chef {
-  constructor() {
+  constructor(chefName, chefRestaurant, morningStatus) {
+    this.name = chefName,
+    this.restaurant = chefRestaurant
   }
-}
+
+  greetCustomer(name, morningStatus) {
+    if (morningStatus) {
+      return `Good morning, ${name}!`
+    } else {
+    return `Hello, ${name}!`
+    }
+  }
+
+  checkForFood(foodItem) {
+    if (this.restaurant.menus[foodItem.type].includes(foodItem)) {
+      return `Yes, we're serving ${foodItem.name} today!`
+    } else {
+    return `Sorry, we aren't serving ${foodItem.name} today.`
+    }
+  }
+};
 
 module.exports = Chef;
