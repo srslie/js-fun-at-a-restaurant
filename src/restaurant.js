1e5437403fc9aab265cc64563a246b3485ad601e
Alice Ruppert <aliceruppert@gmail.com>
2020-10-10 19:24:26 -0600

    Add passing restaurant.js

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,41 @@
+//
+// class Restaurant {
+//   constructor(name, menus) {
+//     this.name = name,
+//     this.menu = menus
+//   }
+// }
 
+function createRestaurant(restaurantName) {
+  var restaurant = {
+    name: restaurantName,
+    menus: {
+      breakfast: [],
+      lunch: [],
+      dinner: [],
+    }
+  }
+  return restaurant
+};
+
+function addMenuItem(restaurant, item) {
+  if (!restaurant.menus[item.type].includes(item)) {
+      restaurant.menus[item.type].push(item)
+  }
+};
+
+function removeMenuItem(restaurant, itemName, itemType) {
+  for (var i = 0; i < restaurant.menus[itemType].length; i++) {
+    if (restaurant.menus[itemType][i].name === itemName) {
+      restaurant.menus[itemType].splice(i, 1)
+    return `No one is eating our ${itemName} - it has been removed from the ${itemType} menu!`
+    }
+  }
+    return `Sorry, we don't sell ${itemName}, try adding a new recipe!`
+};
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
-  // removeMenuItem
+  createRestaurant,
+  addMenuItem,
+  removeMenuItem
 }
